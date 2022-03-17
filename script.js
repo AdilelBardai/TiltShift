a7082f2779b3f04352a860f2261d472866d736d5
AdilelBardai <adil.el.b@hotmail.com>
2022-03-17 13:02:16 +0100

    js file

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var show = document.getElementsByClassName("filterOption");
+
+    if (show.style.display === "none") {
+    show.style.display = "block";
+    } else {
+    show.style.display = "none";
+    }
+}
